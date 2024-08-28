b0ca489ece2b66ecfb4974830c55b3964d30a952
tardigrade34 <muharremacar38@gmail.com>
2024-08-28 07:05:29 +0300

    Update vite.config.js with deploy scripts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/fabriqa/", // Bu, GitHub Pages'deki repository adınıza göre ayarlanmalı
   plugins: [react()],
-})
+});
